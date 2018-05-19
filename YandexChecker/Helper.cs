2deb9288f456960bacba94751eb730134d4cdf5b
Unknown <aumolotok@mail.ru>
2018-05-19 13:45:36 +0300

    Add checking and move to other results tab func

---
+++ YandexChecker/Helper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace YandexChecker
+{
+    static class Helper
+    {
+        public static void WaitForLoad(this IPage page)
+        {
+            int i = 0;
+            while (page.Browser.ExecuteScript("return document.readyState").ToString() != "complete" && i < 5)
+            {
+                i++;
+                Thread.Sleep(1000);     
+            }
+        }
+    }
+}
--- YandexChecker/Program.cs
+++ YandexChecker/Program.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace YandexChecker {
 	class Program {
@@ -15,16 +17,27 @@ namespace YandexChecker {
 			Browser.Navigate().GoToUrl("https://yandex.ru");
 
 			var yandexMain = new YandexPageMain(Browser);
-			yandexMain.Search("Конёк Горбунёк растущий стул");
+            YandexSearchResults yandexResult = yandexMain.Search("Конёк Горбунёк растущий стул");
 
-			Browser.Quit();
+
+            int number = yandexResult.CheckLink("kon-gor.com");
+
+            //yandexResult.WaitForResultsLoad();
+            yandexResult.GoToOtherResultTab();
+            yandexResult.GoToOtherResultTab();
+            yandexResult.GoToOtherResultTab();
+            yandexResult.GoToOtherResultTab();
+            yandexResult.GoToOtherResultTab();
+
+
+            Browser.Quit();
 		}
 	}
 
-	class YandexPageMain {
-		private ChromeDriver Browser;
+	class YandexPageMain : IPage{
+		public ChromeDriver Browser { get; set; }
 
-		public YandexPageMain(ChromeDriver driver)
+        public YandexPageMain(ChromeDriver driver)
 		{
 			Browser = driver;
 		}
@@ -38,12 +51,59 @@ namespace YandexChecker {
 		}
 	}
 
-	class YandexSearchResults {
-		private ChromeDriver Browser;
-
+	class YandexSearchResults: IPage {
 		public YandexSearchResults(ChromeDriver driver)
 		{
 			Browser = driver;
 		}
+
+
+		public ChromeDriver Browser { get; set; }
+
+        public List<IWebElement> Results => Browser.FindElementsByXPath("//li[@class='serp-item']/parent::ul/li[not(@aria-label)]").ToList();
+
+        
+        public int CheckLink(string link)
+        {
+            int i = 1;
+            foreach (var result in Results)
+            {
+                var a = result.FindElement(By.ClassName("link_outer_yes"));
+                if(a.Text == link)
+                {
+                    return i;
+                }
+                i++;
+            }
+            return 0;
+        }
+        
+        public void GoToOtherResultTab() {
+
+            WaitForResultsLoad();
+
+            WebDriverWait wait = new WebDriverWait(Browser, TimeSpan.FromSeconds(30));
+            wait.Until(d => d.FindElement(By.XPath("//div[@aria-label = 'Страницы']/a[text() = 'дальше']")).Displayed == true);
+            IWebElement ContinueButton = Browser.FindElementByXPath("//div[@aria-label = 'Страницы']/a[text() = 'дальше']");
+            ContinueButton.Click();
+        }
+
+        public void WaitForResultsLoad()
+        {   
+            for(int i = 0; i < 100; i++)
+            {
+                if (Browser.FindElementByXPath("//body").GetAttribute("aria-busy") == null)
+                {
+                    break;
+                }
+
+                if (Browser.FindElementByXPath("//body").GetAttribute("aria-busy").ToString() == "true")
+                {
+                    Thread.Sleep(100);
+                }
+            } 
+        }
 	}
+
+
 }
